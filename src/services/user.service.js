20db808f5a300ec074e85b24c64b7221ed6b3b5c
MohamedGoharFCIH <engmabdo85@gmail.com>
2022-08-01 14:05:37 +0200

    implement functions getMedicines & getPatients in UserService

--- src/services/user.service.js
+++ src/services/user.service.js
@@ -1,19 +1,34 @@
 import axios from "axios";
 import authHeader from "./auth-header";
 
-const API_URL = process.env.API_URL;
+const API_URL = process.env.REACT_APP_API_URL;
+
+const getUser = () => {
+    return axios.get(API_URL + "user/", { headers: authHeader() });
+}
 
 const getUsers = () => {
-    return axios.get(API_URL + "users", { headers: authHeader() });
+    return axios.get(API_URL + "users/", { headers: authHeader() });
 };
+
+const getPatients = () => {
+    return axios.get(API_URL + "patients/", { headers : authHeader() })
+}
 
 const updateUser = (user) => {
-    return axios.put(API_URL + "user", user, { headers: authHeader() });
+    return axios.put(API_URL + "user/", user, { headers: authHeader() });
 };
 
+const getMedicines = () => {
+    return axios.get(API_URL + "medicines/", { headers: authHeader() })
+}
+
 const UserService = {
-   getUsers,
-   updateUser
+    getUser,
+    getUsers,
+    updateUser,
+    getMedicines,
+    getPatients
 };
 
 export default UserService;
